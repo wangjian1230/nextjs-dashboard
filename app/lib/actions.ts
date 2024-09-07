f497879646a5f7573d2eba16a9b8aee898b141f4
隔壁老王 <16436900+wangjian1230@users.noreply.github.com>
2024-09-07 15:52:48 +0800

    over

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -130,7 +130,6 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice');
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
   } catch (e) {
